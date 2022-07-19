7f76b8aede4f9b63188c52b1e04f4c9acc04c1db
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-07-19 21:09:26 +0530

    n=int(input()) a=list(map(int,input().split())) k=int(input()) c=0 for i in a:     if(i==k):         c+=1         break if(c==1):     print("True") else:     print("False")

---
+++ element_search_in_an_array.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in a:
+    if(i==k):
+        c+=1
+        break
+if(c==1):
+    print("True")
+else:
+    print("False")
+        

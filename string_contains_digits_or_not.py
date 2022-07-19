3fb69dcace41268aaff7140d41a428192cb39082
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-07-19 11:17:05 +0530

    n=int(input()) for _ in range(n):     s=input()     c=0     for i in s:         if(i.isdigit()):             c+=1     if(c>0):         print("Yes")     else:         print("No")

---
+++ string_contains_digits_or_not.py
@@ -0,0 +1,12 @@
+n=int(input())
+for _ in range(n):
+    s=input()
+    c=0
+    for i in s:
+        if(i.isdigit()):
+            c+=1
+    if(c>0):
+        print("Yes")
+    else:
+        print("No")
+        

63c2df16cd482be18fbc074ca62c0a24f3e2c7dd
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-07-19 22:44:11 +0530

    def rev(n):     rev=0     while(n):         r=n%10         n=n//10         rev=rev*10+r     return rev n=int(input()) a=list(map(int,input().split())) c=0 for i in a:     if(i==rev(i)):         c+=1 print(c)

---
+++ Count_palindromes.py
@@ -0,0 +1,14 @@
+def rev(n):
+    rev=0
+    while(n):
+        r=n%10
+        n=n//10
+        rev=rev*10+r
+    return rev    
+n=int(input())
+a=list(map(int,input().split()))
+c=0
+for i in a:
+    if(i==rev(i)):
+        c+=1
+print(c)        

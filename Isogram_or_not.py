1c25a746b4506469514421f6e40ce1fbc7a37de9
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-09-07 22:46:44 +0530

    s=input() k=set(s) if(len(s)==len(k)):     print("True") else:     print("False")

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,10 +1,6 @@
 s=input()
-b=[]
-c=0
-for i in s:
-    if i in s and i not in b:
-        b.append(i)
-if(len(s)==len(b)):
+k=set(s)
+if(len(s)==len(k)):
     print("True")
 else:
     print("False")

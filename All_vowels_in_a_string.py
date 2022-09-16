9fa0cc43af85adbefef13a17e738f8180e16fbd6
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-09-16 21:45:00 +0530

    s=input() v="aeiou" b=[] for i in s:     if i in v and i not in b:         b.append(i) if(len(b)==len(v)):     print("True") else:     print("False")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,10 +1,10 @@
 s=input()
-v="aeoiu"
+v="aeiou"
 b=[]
 for i in s:
     if i in v and i not in b:
         b.append(i)
-if len(v)==len(b):
-    print(True)
+if(len(b)==len(v)):
+    print("True")
 else:
-    print(False)
+    print("False")

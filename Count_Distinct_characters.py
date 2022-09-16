1e1255d97970a3bd54b9ff22d94c238a6d96ef01
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-09-16 23:03:13 +0530

    s=input().lower() c=0 b=[] for i in s:     if i not in b and i!=' ':         b.append(i)         c+=1 print(c)

--- Count_Distinct_characters.py
+++ Count_Distinct_characters.py
@@ -1,8 +1,8 @@
-s=input()
-s=s.lower()
-k=set(s)
-g=[]
-for i in k:
-    if i!=" ":
-        g.append(i)
-print(len(g))
+s=input().lower()
+c=0
+b=[]
+for i in s:
+    if i not in b and i!=' ':
+        b.append(i)
+        c+=1
+print(c)        

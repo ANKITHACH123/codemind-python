74d2e859e5217d58bc57eb9024474db36e445373
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-09-17 21:01:40 +0530

    n,m=map(int,input().split()) l=list(map(int,input().split())) k=list(map(int,input().split())) s=[] for i in l:     if i not in k and i not in s and i!=' ':         s.append(i) for i in k:     if i not in l and i not in s and i!=' ':         s.append(i) for i in s:     print(i,end=' ')

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,13 +1,12 @@
-a,b=map(int,input().split())
-x=list(map(int,input().split()))
-y=list(map(int,input().split()))
-m=[]
-for i in x:
-    if i not in y:
-        if i not in m:
-            m.append(i)
-for j in y:
-    if j not in x:
-        if j not in m:
-            m.append(j)
-print(*m)
+n,m=map(int,input().split())
+l=list(map(int,input().split()))
+k=list(map(int,input().split()))
+s=[]
+for i in l:
+    if i not in k and i not in s and i!=' ':
+        s.append(i)
+for i in k:
+    if i not in l and i not in s and i!=' ':
+        s.append(i)
+for i in s:
+    print(i,end=' ')

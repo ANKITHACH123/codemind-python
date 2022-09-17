f112ffa71aa2fcdd87df7426038c5720eb203358
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-09-17 22:18:33 +0530

    n,m=map(int,input().split()) l=list(map(int,input().split())) k=list(map(int,input().split())) c=[] d=0 for i in l:     if i not in c and i not in k and i!=' ':         c.append(i) for i in k:     if i not in c and i not in l and i!=' ':         c.append(i) for i in c:     d+=1 print(d)

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,15 +1,14 @@
-a,b=map(int,input().split())
-x=list(map(int,input().split()))
-y=list(map(int,input().split()))
-c=set(x)
-d=set(y)
-s=0
-k=0
-count=0
+n,m=map(int,input().split())
+l=list(map(int,input().split()))
+k=list(map(int,input().split()))
+c=[]
+d=0
+for i in l:
+    if i not in c and i not in k and i!=' ':
+        c.append(i)
+for i in k:
+    if i not in c and i not in l and i!=' ':
+        c.append(i)
 for i in c:
-    if i not in d:
-        s+=1
-for j in d:
-    if j not in c:
-        k+=1
-print(s+k)
+    d+=1
+print(d)  

4310134dfa2d97d156e7783d72fcd56264ecfdea
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-09-19 21:54:45 +0530

    l=list(map(str,input().split())) d=[] for i in l:     c=0     for j in i:         if j in "aeiouAEOIU":             c+=1     d.append(c) k=[] for i in d:     if i==min(d):         k.append(i) print(len(k))

--- minimum_vowel_count_words.py
+++ minimum_vowel_count_words.py
@@ -1,11 +1,13 @@
-s=input().split()
-v="aeiou"
-l=[]
-for i in s:
+l=list(map(str,input().split()))
+d=[]
+for i in l:
     c=0
     for j in i:
-        if j in v:
+        if j in "aeiouAEOIU":
             c+=1
-    l.append(c)
-print(l.count(min(l)))    
-        
+    d.append(c)    
+k=[]
+for i in d:
+    if i==min(d):
+        k.append(i)
+print(len(k))        

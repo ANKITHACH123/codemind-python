2608edda1db2357aa2abab71d2afea365455adc1
ANKITHACH123 <98959169+ANKITHACH123@users.noreply.github.com>
2022-09-20 22:45:48 +0530

    n=input() k="aeiouAEIOU" b=[] for i in n:     if i in k and i not in b:         b.append(i) print(*b)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,7 +1,7 @@
-s=input()
-v="aeiouAEIOU"
-l=[]
-for i in s:
-    if i in v and i not in b:
-        l.append(i)
-print(*l)
+n=input()
+k="aeiouAEIOU"
+b=[]
+for i in n:
+    if i in k and i not in b:
+        b.append(i)
+print(*b)
